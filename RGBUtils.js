fad2ab82d0252244858740a03327e5567003162c
erictktk <etdeveloper86@gmail.com>
2023-12-28 17:42:56 -0500

    separated hsv from hsl

--- RGBUtils.js
+++ RGBUtils.js
@@ -1,286 +1,241 @@
+function Clamp(value, min, max){
+    return Math.max(min, Math.min(value, max));
+}
+
 /**
- * Returns h [0, 360), s [0, 100), v [0, 100)
+ * Converts RGB to HSL color format.
+ * 
+ * @param {Array<number>} color - RGB(A) color [R, G, B, (optional) A], R/G/B as 0-255, A as 0-1.
+ * @param {Boolean} return01 - keeps values in p0, 1] range
+ * @returns {Array<number>} HSL color [H, S, L], H in degrees (0-360), S and L in (0-100)
+ */
+export function RGBtoHSL(color, return01=false) {
+    let [r, g, b, a] = [...color];
+    (r /= 255), (g /= 255), (b /= 255);
+    const vmax = Math.max(r, g, b),
+        vmin = Math.min(r, g, b);
+    let h,
+        s,
+        l = (vmax + vmin) / 2;
+
+    if (vmax === vmin) {
+        return [0, 0, l]; // achromatic
+    }
+
+    const d = vmax - vmin;
+    s = l > 0.5 ? d / (2 - vmax - vmin) : d / (vmax + vmin);
+    if (vmax === r) h = (g - b) / d + (g < b ? 6 : 0);
+    if (vmax === g) h = (b - r) / d + 2;
+    if (vmax === b) h = (r - g) / d + 4;
+    h /= 6;
+
+    if (!return01){
+        return [h*360, s*100, l*100];
+    }
+    else{
+        return [h, s, l];
+    }
+}
+
+/** 
+ * Converts an HSL color value to RGB
+ * Assumes h range = [0, 360]  s and l range = [0, 100]
+ * returns r, g, and b in the set [0, 255].
+ * 
+ * @param {Number} h 
+ * @param {Number} s 
+ * @param {Number} l 
+ */
+export function HSLtoRGB(h, s, l){
+    const [h2, s2, l2] = [h/360.0, s/100.0, l/100.0];
+    return HSLtoRGB(h2, s2, l2);
+}
+
+
+/**
+ * Converts an HSL color value to RGB. Conversion formula
+ * adapted from https://en.wikipedia.org/wiki/HSL_color_space.
+ * Assumes h range = [0, 360]  s and l range = [0, 100]
+ * returns r, g, and b in the set [0, 255].
  *
- * @param {Array<int>} color
+ * @param   {number}  h       The hue
+ * @param   {number}  s       The saturation
+ * @param   {number}  l       The lightness
+ * @return  {Array}           The RGB representation
+ */
+export function HSLtoRGB01(h, s, l) {
+    let r, g, b;
+
+    if (s === 0) {
+        r = g = b = l; // achromatic
+    } else {
+        const q = l < 0.5 ? l * (1 + s) : l + s - l * s;
+        const p = 2 * l - q;
+        r = hueToRgb(p, q, h + 1 / 3);
+        g = hueToRgb(p, q, h);
+        b = hueToRgb(p, q, h - 1 / 3);
+    }
+
+    return [Math.round(r * 255), Math.round(g * 255), Math.round(b * 255)];
+}
+
+function hueToRgb(p, q, t) {
+    if (t < 0) t += 1;
+    if (t > 1) t -= 1;
+    if (t < 1 / 6) return p + (q - p) * 6 * t;
+    if (t < 1 / 2) return q;
+    if (t < 2 / 3) return p + (q - p) * (2 / 3 - t) * 6;
+    return p;
+}
+
+/**
+ *
+ * @param {Array<Int>} color an array of 4 or 5
+ * @param {Number} hueMod from -360 to 360
+ * @param {Number} saturateMod from -100 to 100
+ * @param {Number} lightnessMod from -100 to 100
+ */
+export function HSLMod(color, hueMod = 0, saturateMod = 0, lightnessMod = 0) {
+    let [r, g, b, a] = [...color];
+    let [h, s, l] = RGBtoHSL([r, g, b], true);
+
+    h = Math.clamp(h + hueMod);
+    s = Math.clamp(s + saturateMod);
+    l = Math.clamp(l + lightnessMod);
+
+    let [r2, g2, b2] = HSLtoRGB01([h, s, l]);
+
+    if (a) {
+        return [r2, g2, b2, a];
+    } else {
+        return [r2, g2, b2];
+    }
+}
+//#endregion
+
+
+
+/** Mods Hue values in range 0 to 360
+ *
+ * @param {Array<Number>} rgb
+ * @param {Number} hueRotate range is [0, 360]
  * @returns {Array<Number>}
  */
-export function rgb_to_hsv(color) {
-  let r = color[0] / 255.0;
-  let g = color[1] / 255.0;
-  let b = color[2] / 255.0;
+export function RGBModHue(rgb, hueRotate) {
+    let hsv = RGBtoHSL(rgb);
 
-  let cmax = Math.max(r, Math.max(g, b));
-  let cmin = Math.min(r, Math.min(g, b));
-  let diff = cmax - cmin;
-  let h = -1,
-    s = -1;
+    hsv[0] += hueRotate;
 
-  if (cmax === cmin) {
-    h = 0;
-  } else if (cmax === r) {
-    h = (60 * ((g - b) / diff) + 360) % 360;
-  } else if (cmax === g) {
-    h = (60 * ((b - r) / diff) + 120) % 360;
-  } else if (cmax === b) {
-    h = (60 * ((r - g) / diff) + 240) % 360;
-  }
+    let newRGB3 = HSLtoRGB(hsv);
+    let newRGB;
 
-  s = cmax === 0 ? 0 : (diff / cmax) * 100;
-  let v = cmax * 100;
-
-  return [h, s, v];
+    if (rgb.length === 4) {
+        newRGB = [newRGB3[0], newRGB3[1], newRGB3[2], rgb[3]];
+    } else {
+        newRGB = [newRGB3[0], newRGB3[1], newRGB3[2]];
+    }
+    return newRGB;
 }
 
-/**
- * HSV should be [0, 360], [0, 100] and [0, 100]
+/** Mods Hue values in range 0 to 100
  *
- * Returns an Array of RGB values
- *
- * @param {Array<int>} hsv
+ * @param {Array<Number>} rgb
+ * @param {Number} hueRotate range is [0, 360]
  * @returns {Array<Number>}
  */
-export function hsv_to_rgb(hsv) {
-  let h = hsv[0] / 360;
-  let s = hsv[1] / 100;
-  let v = hsv[2] / 100;
+export function RGBModLightness(rgb, value) {
+    let hsv = RGBtoHSL(rgb);
 
-  let r, g, b, i, f, p, q, t;
+    hsv[2] = Math.min(Math.max(hsv[2] + value, 0), 100);
 
-  if (arguments.length === 1) {
-    s = hsv.s / 100;
-    v = hsv.v / 100;
-    h = hsv.h / 360;
-  }
+    console.log(hsv);
 
-  i = Math.floor(h * 6);
-  f = h * 6 - i;
-  p = v * (1 - s);
-  q = v * (1 - f * s);
-  t = v * (1 - (1 - f) * s);
+    let newRGB3 = HSLtoRGB(hsv);
+    let newRGB;
 
-  switch (i % 6) {
-    case 0:
-      r = v;
-      g = t;
-      b = p;
-      break;
-    case 1:
-      r = q;
-      g = v;
-      b = p;
-      break;
-    case 2:
-      r = p;
-      g = v;
-      b = t;
-      break;
-    case 3:
-      r = p;
-      g = q;
-      b = v;
-      break;
-    case 4:
-      r = t;
-      g = p;
-      b = v;
-      break;
-    case 5:
-      r = v;
-      g = p;
-      b = q;
-      break;
-  }
+    if (rgb.length === 4) {
+        newRGB = [newRGB3[0], newRGB3[1], newRGB3[2], rgb[3]];
+    } else {
+        newRGB = [newRGB3[0], newRGB3[1], newRGB3[2]];
+    }
 
-  return [Math.round(r * 255), Math.round(g * 255), Math.round(b * 255)];
+    return newRGB;
 }
 
-export function hsv2rgb(hsv) {
-  let h = hsv[0];
-  let s = hsv[1] / 100;
-  let v = hsv[2] / 100;
 
-  let f = (n, k = (n + h / 60) % 6) =>
-    v - v * s * Math.max(Math.min(k, 4 - k, 1), 0);
-
-  return [f(5) * 255, f(3) * 255, f(1) * 255];
-}
-
-/**
- * A faster, all-together mod
+/** Sets Lightness values in range [0, 100]
  *
- * @param {Array<int>} rgb
- * @param {Number} hueMod
- * @param {Number} saturateMod
- * @param {Number} valueMod
- * @returns {Array<Number>} an array of new RGB values
- */
-export function RGBModHSV(rgb, hueMod, saturateMod, valueMod) {
-  let hsv = rgb_to_hsv(rgb);
-
-  hsv[0] = hueMod;
-  hsv[1] = Math.min(Math.max(hsv[1] + saturateMod, 0), 100);
-  hsv[2] = Math.min(Math.max(hsv[2] + valueMod, 0), 100);
-
-  let newRGB3 = hsv2rgb(hsv);
-  let newRGB;
-
-  if (rgb.length === 4) {
-    newRGB = [newRGB3[0], newRGB3[1], newRGB3[2], rgb[3]];
-  } else {
-    newRGB = [newRGB3[0], newRGB3[1], newRGB3[2]];
-  }
-
-  return newRGB;
-}
-
-/**
- * @param {Array<int>} rgb
- * @param {Number} hueRotate what should the range be
+ * @param {Array<Number>} rgb
+ * @param {Number} lightness range is [0, 100]
  * @returns {Array<Number>}
  */
-export function RGBHueRotate(rgb, hueRotate) {
-  let hsv = rgb_to_hsv(rgb);
+export function RGBSetLightness(rgb, lightness) {
+    let hsl = RGBtoHSL(rgb);
 
-  hsv[0] += hueRotate;
+    hsl[2] = lightness;
 
-  let newRGB3 = hsv2rgb(hsv);
-  let newRGB;
+    //console.log(hsv);
 
-  if (rgb.length === 4) {
-    newRGB = [newRGB3[0], newRGB3[1], newRGB3[2], rgb[3]];
-  } else {
-    newRGB = [newRGB3[0], newRGB3[1], newRGB3[2]];
-  }
-  return newRGB;
+    let newRGB3 = HSLtoRGB(hsl);
+    let newRGB;
+
+    if (rgb.length === 4) {
+        newRGB = [newRGB3[0], newRGB3[1], newRGB3[2], rgb[3]];
+    } else {
+        newRGB = [newRGB3[0], newRGB3[1], newRGB3[2]];
+    }
+
+    return newRGB;
 }
 
-export function RGBModSaturate(rgb, amount) {
-  let hsv = rgb_to_hsv(rgb);
-  console.log("desaturate:");
-  console.log(hsv);
-  hsv[1] = hsv[1] + amount;
-
-  let newRGB3 = hsv2rgb(hsv);
-  let newRGB;
-
-  if (rgb.length === 4) {
-    newRGB = [newRGB3[0], newRGB3[1], newRGB3[2], rgb[3]];
-  } else {
-    newRGB = [newRGB3[0], newRGB3[1], newRGB3[2]];
-  }
-
-  return newRGB;
-}
-
-/** Going to be deprecated */
-export function RGBBrighten(rgb, amount) {
-  return RGBModBrightness(rgb, amount);
-}
-
-export function RGBModBrightness(rgb, amount) {
-  let hsv = rgb_to_hsv(rgb);
-
-  hsv[2] = Math.min(Math.max(hsv[2] + amount, 0), 100);
-
-  console.log(hsv);
-
-  let newRGB3 = hsv2rgb(hsv);
-  let newRGB;
-
-  if (rgb.length === 4) {
-    newRGB = [newRGB3[0], newRGB3[1], newRGB3[2], rgb[3]];
-  } else {
-    newRGB = [newRGB3[0], newRGB3[1], newRGB3[2]];
-  }
-
-  return newRGB;
-}
-
-export function RGBDesaturate(rgb, desaturation = 50) {
-  let hsv = rgb_to_hsv(rgb);
-  console.log("desaturate:");
-  console.log(hsv);
-  hsv[1] = hsv[1] - desaturation;
-
-  console.log(hsv);
-
-  let newRGB3 = hsv2rgb(hsv);
-  let newRGB;
-
-  if (rgb.length === 4) {
-    newRGB = [newRGB3[0], newRGB3[1], newRGB3[2], rgb[3]];
-  } else {
-    newRGB = [newRGB3[0], newRGB3[1], newRGB3[2]];
-  }
-
-  return newRGB;
-}
-
-/**
- * @param {Array<int>} rgb
- * @param {Number} brightness Value between 0 and 100
- * @returns {Array<Number>} - an array of length 3 or 4 depending on the input
+/** Mods Saturation values in range [0, 100]
+ *
+ * @param {Array<Number>} rgb
+ * @param {Number} saturation range is [0, 100]
+ * @returns Array<Number>
  */
-export function RGBSetBrightness(rgb, brightness) {
-  let hsv = rgb_to_hsv(rgb);
+export function RGBModSaturation(rgb, saturation = 20) {
+    let hsl = RGBtoHSL(rgb);
+    hsl[1] = hsl[1] + saturation;
+    
+    hsl[1] = Clamp(hsl[1], 0, 100);
 
-  hsv[2] = brightness;
+    let newRGB3 = HSLtoRGB(hsl);
+    let newRGB;
 
-  let newRGB3 = hsv2rgb(hsv);
-  let newRGB;
+    if (rgb.length === 4) {
+        newRGB = [newRGB3[0], newRGB3[1], newRGB3[2], rgb[3]];
+    } else {
+        newRGB = [newRGB3[0], newRGB3[1], newRGB3[2]];
+    }
 
-  if (rgb.length === 4) {
-    newRGB = [newRGB3[0], newRGB3[1], newRGB3[2], rgb[3]];
-  } else {
-    newRGB = [newRGB3[0], newRGB3[1], newRGB3[2]];
-  }
-
-  return newRGB;
+    return newRGB;
 }
 
-/**
- * @param {Array<int>} rgb
- * @param {Number} saturation Value between 0 and 100
- * @returns {Array<Number>} - an array of length 3 or 4 depending on the input
- */
-export function RGBSetSaturation(rgb, saturation) {
-  let hsv = rgb_to_hsv(rgb);
-
-  // Set saturation
-  hsv[1] = Math.min(Math.max(saturation, 0), 100);
-
-  let newRGB3 = hsv2rgb(hsv);
-  let newRGB;
-
-  if (rgb.length === 4) {
-    newRGB = [newRGB3[0], newRGB3[1], newRGB3[2], rgb[3]];
-  } else {
-    newRGB = [newRGB3[0], newRGB3[1], newRGB3[2]];
-  }
-
-  return newRGB;
-}
-
-/**
- * @param {Array<int>} rgb
- * @param {Number} hue Value between 0 and 360
+/** Sets Saturation values in range [0, 100]
+ *
+ * @param {Array<Number>} rgb
+ * @param {Number} saturation range is [0, 100]
  * @returns {Array<Number>}
  */
-export function RGBSetHue(rgb, hue) {
-  let hsv = rgb_to_hsv(rgb);
-  
+export function RGBSetSaturation(rgb, saturation = 60) {
+    let hsl = RGBtoHSL(rgb);
+    hsl[1] = saturation;
 
-  // Set hue
-  hsv[0] = hue % 360;
+    let newRGB3 = HSLtoRGB(hsl);
+    let newRGB;
 
-  let newRGB3 = hsv2rgb(hsv);
-  let newRGB;
+    if (rgb.length === 4) {
+        newRGB = [newRGB3[0], newRGB3[1], newRGB3[2], rgb[3]];
+    } else {
+        newRGB = [newRGB3[0], newRGB3[1], newRGB3[2]];
+    }
 
-  if (rgb.length === 4) {
-    newRGB = [newRGB3[0], newRGB3[1], newRGB3[2], rgb[3]];
-  } else {
-    newRGB = [newRGB3[0], newRGB3[1], newRGB3[2]];
-  }
+    return newRGB;
+}
 
-  return newRGB;
+export function GetLuminance(rgb) {
+    const hsl = RGBtoHSL(rgb);
+
+    return hsl[2];
 }
